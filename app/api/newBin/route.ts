e36403cefc0e8315448033099e40bef87c3a712e
pikkauhn <114180449+pikkauhn@users.noreply.github.com>
2024-03-14 09:22:51 -0500

    Update route.ts

--- app/api/newBin/route.ts
+++ app/api/newBin/route.ts
@@ -5,20 +5,20 @@ const prisma = new PrismaClient();
 
 interface Bin {
     size: string,
-    binSizeId: string
+    binSizeId: string,
+    charge: number
 }
 
 export async function POST(req: Request) {
     const body: Bin = await req.json();
     console.log(body)
     try {
-        const result = await prisma.bin.create({
-            data: {
-                binSizeId: body.binSizeId,
-                status: 'Inactive',
-            }
-        });
-        return NextResponse.json(result, { status: 200 });
+        // const result = await prisma.bin.create({
+        //     data: {                
+                
+        //     }
+        // });
+        // return NextResponse.json(result, { status: 200 });
     } catch (error) {
         console.error('Error: ', error);
         return NextResponse.json({ message: 'Failed to create data' }, { status: 500 });
